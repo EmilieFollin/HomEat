51a0ae1606f7cf8f4fd0b924679ca91f8110f73d
EmilieFollin <EmilieFollin@users.noreply.github.com>
2018-03-21 14:15:29 +0100

    MAJ du 21/03

---
+++ public/js/index/template.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    console.log('CHARGEMENT   :    TEMPLATE.JS');
+
+    // INUTILISE ACTUELLEMENT
+
+        $('#submit-template').click(function(e) {
+            //e.preventDefault();
+            //location.href = 'http://localhost:8000/personnalisation/changement-de-template';
+    })
+
+})
